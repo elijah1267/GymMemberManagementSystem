f1fb010b8791028243a60cb937aa38c993b5102c
elijah12 <elijah12@naver.com>
2022-03-13 20:18:34 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Members");
+		System.out.println("1. Delete Members");
+		System.out.println("1. Edit Members");
+		System.out.println("1. View Members");
+		System.out.println("1. Exit");
+
+	}
+
+}
