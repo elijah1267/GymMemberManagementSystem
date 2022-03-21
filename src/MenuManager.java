41a69a9518f0bee0d4b543e411a42d5cd0aa76b3
elijah12 <elijah12@naver.com>
2022-03-21 14:54:42 +0900

    Add Menu

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,12 +1,51 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Members");
-		System.out.println("2. Delete Members");
-		System.out.println("3. Edit Members");
-		System.out.println("4. View Members");
-		System.out.println("5. Exit");
+		
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while (num != 6) {
+			
+			System.out.println("1. Add Members");
+			System.out.println("2. Delete Members");
+			System.out.println("3. Edit Members");
+			System.out.println("4. View Members");
+			System.out.println("5. Show a Menu");
+			System.out.println("6. Exit");
+			System.out.print("Select one number between 1 - 6 : ");
+			num = input.nextInt();
+			
+			switch (num) {
+			case 1:
+				System.out.print("Member Id : ");
+				int Id = input.nextInt();
+				System.out.print("Name : ");
+				String Name = input.next();
+				System.out.print("Phone number : ");
+				String Pn = input.next();
+				System.out.print("Address : ");
+				String Add = input.next();
+				break;
+			case 2:
+				System.out.print("Member Id : ");
+				int Id2 = input.nextInt();
+				break;
+			case 3:
+				System.out.print("Member Id : ");
+				int Id3 = input.nextInt();
+				break;
+			case 4:
+				System.out.print("Member Id : ");
+				int Id4 = input.nextInt();
+				break;
+			}
+			
+			
+		}
+		
 
 	}
 
